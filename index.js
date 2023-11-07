8e3c25115f9d43a28c0b2b369b5abb244a3b224d
JohnB08 <johnb08.kodehode@gmail.com>
2023-11-07 19:04:43 +0100

    adda comments som forklarer hva den gjør.

--- index.js
+++ index.js
@@ -5,19 +5,26 @@ const fileSaveArray = [];
 //Lager en funksjon som tar inn alle navnene fra filene lagt inn, og spytter ut en copypasteable string som kan limes inn i andre dokumenter.
 function uploadFiles(event) {
   event.preventDefault();
+  //henter in input element fra html
   const fileInput = document.querySelector("#fileInput");
+  //input.files er alle filene som er lagt inn i input elementet når knappen er trykket.
   const selectedFiles = fileInput.files;
   console.log(selectedFiles);
+  //dette er en fileList, som ikke funker *helt* som et array, men kan loopes gjennom med vanlig for løkke.
   for (let i = 0; i < selectedFiles.length; i++) {
+    //pusher navnet til hver fil inn i fileSaveArray
     fileSaveArray.push(selectedFiles[i].name);
   }
+  //lager en tom string
   let arrayString = "";
+  //lager en string basert på filenames
   fileSaveArray.forEach((fileName) => {
     arrayString += `"${fileName}", `;
   });
   console.log(arrayString);
+  //lager et p element, putter inn den komplette arraystringen inn i pseudokoden og appender det til formen.
   let arrayText = document.createElement("p");
-  arrayText.textContent = `const fileNameArray = [${arrayString}]`;
+  arrayText.textContent = `const fileNameArray = [${arrayString}];`;
   fileUploadForm.appendChild(arrayText);
   console.log(fileSaveArray);
 }
