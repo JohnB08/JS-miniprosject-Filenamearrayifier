bb3408f9aa6cb0665ab02e227509248a0d3f472a
JohnB08 <johnb08.kodehode@gmail.com>
2023-11-07 18:47:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const uploadBtn = document.querySelector("#uploadBtn");
+//lager et tomt fileSaveArray
+const fileSaveArray = [];
+//Lager en funksjon som tar inn alle navnene fra filene lagt inn, og spytter ut en copypasteable string som kan limes inn i andre dokumenter.
+function uploadFiles(event) {
+  event.preventDefault();
+  const fileInput = document.querySelector("#fileInput");
+  const selectedFiles = fileInput.files;
+  console.log(selectedFiles);
+  for (let i = 0; i < selectedFiles.length; i++) {
+    fileSaveArray.push(selectedFiles[i].name);
+  }
+  let arrayString = "";
+  fileSaveArray.forEach((fileName) => {
+    arrayString += `"${fileName}",`;
+  });
+  console.log(arrayString);
+  let arrayText = document.createElement("p");
+  arrayText.textContent = `const fileNameArray = [${arrayString}]`;
+  document.body.appendChild(arrayText);
+  console.log(fileSaveArray);
+}
+//eventlistener på knapp.
+uploadBtn.addEventListener("click", uploadFiles);
