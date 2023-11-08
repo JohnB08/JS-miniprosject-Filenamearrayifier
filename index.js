fac0ebeaf87eb8c8d080d7e119b0f0480185d01c
JohnB08 <johnb08.kodehode@gmail.com>
2023-11-08 09:25:35 +0100

    added object function

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
 const uploadBtn = document.querySelector("#uploadBtn");
 const fileUploadForm = document.querySelector("#fileUpload");
+const fileInput = document.querySelector("#fileInput");
+const functionSelect = document.querySelector("#functionSelect");
+const folderLabel = document.createElement("label");
+folderLabel.setAttribute("for", "folderNameInput");
+folderLabel.setAttribute("id", "folderLabel");
+folderLabel.textContent = "Set a foldername!";
+const folderNameInput = document.createElement("input");
+folderNameInput.setAttribute("type", "text");
+folderNameInput.setAttribute("id", "folderNameInput");
+folderLabel.appendChild(folderNameInput);
+const outputText = document.createElement("p");
+fileUploadForm.appendChild(outputText);
+
 //lager et tomt fileSaveArray
-const fileSaveArray = [];
-//Lager en funksjon som tar inn alle navnene fra filene lagt inn, og spytter ut en copypasteable string som kan limes inn i andre dokumenter.
-function uploadFiles(event) {
-  event.preventDefault();
-  //henter in input element fra html
-  const fileInput = document.querySelector("#fileInput");
+
+function getFiles() {
+  let fileNameArray = [];
   //input.files er alle filene som er lagt inn i input elementet når knappen er trykket.
   const selectedFiles = fileInput.files;
-  console.log(selectedFiles);
   //dette er en fileList, som ikke funker *helt* som et array, men kan loopes gjennom med vanlig for løkke.
   for (let i = 0; i < selectedFiles.length; i++) {
     //pusher navnet til hver fil inn i fileSaveArray
-    fileSaveArray.push(selectedFiles[i].name);
+    fileNameArray.push(selectedFiles[i].name);
+    //lager en tom string
   }
-  //lager en tom string
-  let arrayString = "";
+  return fileNameArray;
+}
+//Lager en funksjon som tar inn alle navnene fra filene lagt inn, og spytter ut en copypasteable string som kan limes inn i andre dokumenter.
+function arrayify(event) {
+  event.preventDefault();
+  //henter in input element fra html
+  let fileNameArray = getFiles();
+  let stringifiedArray = "";
   //lager en string basert på filenames
-  fileSaveArray.forEach((fileName) => {
-    arrayString += `"${fileName}", `;
+  fileNameArray.forEach((fileName) => {
+    stringifiedArray += `"${fileName}", `;
   });
-  console.log(arrayString);
   //lager et p element, putter inn den komplette arraystringen inn i pseudokoden og appender det til formen.
-  let arrayText = document.createElement("p");
-  arrayText.textContent = `const fileNameArray = [${arrayString}];`;
-  fileUploadForm.appendChild(arrayText);
-  console.log(fileSaveArray);
+  outputText.textContent = `const fileNameArray = [${stringifiedArray}];`;
+  console.log(fileNameArray);
+}
+
+function objectify(event) {
+  event.preventDefault();
+  let fileNameArray = getFiles();
+  let stringifiedObject = "";
+  fileNameArray.forEach((fileName) => {
+    stringifiedObject += `${fileName.slice(0, -4)}:{filename: "${fileName}"}, `;
+  });
+  outputText.textContent = `const fileNameObject = {folder: "./${folderNameInput.value}/", ${stringifiedObject}}`;
 }
 //eventlistener på knapp.
-uploadBtn.addEventListener("click", uploadFiles);
+uploadBtn.addEventListener("click", (event) => {
+  if (functionSelect.value === "Object") objectify(event);
+  else arrayify(event);
+});
+// Skifte mellom array og Object.
+functionSelect.addEventListener("change", () => {
+  if (functionSelect.value === "Object")
+    fileUploadForm.appendChild(folderLabel);
+  else {
+    let currentInput = document.querySelector("#folderLabel");
+    if (currentInput) currentInput.remove();
+  }
+});
